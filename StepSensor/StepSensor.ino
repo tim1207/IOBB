b386109cbff551755ec568901d34c9292d0a51fa
tim1207 <timlin891207@gmail.com>
2021-12-02 18:09:31 +0800

    modfiy and add time in arduino code

--- StepSensor/StepSensor.ino
+++ StepSensor/StepSensor.ino
@@ -9,99 +9,152 @@ BluetoothSerial BT;
 //#include <SoftwareSerial.h> //Serial transport for bluetooth
 //SoftwareSerial AT; //set bluetooth name as BT
 BNO080 myIMU;
+unsigned long myTime;
+unsigned long myTime2;
 
 void setup()
 {
   Serial.begin(115200);
- // AT.begin(115200);
-  BT.begin("hand");
-  BT.println("Welcome !!!!");
   Serial.println();
   Serial.println("BNO080 Read Example");
+  BT.begin("KWK");
 
   Wire.begin();
 
   myIMU.begin();
 
   Wire.setClock(400000); //Increase I2C data rate to 400kHz
-  myIMU.enableGyro(50);
-  myIMU.enableMagnetometer(50); //Send data update every 50ms
 
-  Serial.println(F("Magnetometer enabled"));
+  myIMU.enableGyro(50); //Send data update every 50ms
+
+  myIMU.enableAccelerometer(50);
+
+  //Enable dynamic calibration for accel, gyro, and mag
+  myIMU.calibrateAll(); //Turn on cal for Accel, Gyro, and Mag
+
+  //Enable Game Rotation Vector output
+  myIMU.enableGameRotationVector(100); //Send data update every 100ms
+
+  //Enable Magnetic Field output
+  myIMU.enableMagnetometer(100); //Send data update every 100ms
+
+  
+
+  //Once magnetic field is 2 or 3, run the Save DCD Now command
+  Serial.println(F("Calibrating. Press 's' to save to flash"));
   Serial.println(F("Output in form x, y, z, in uTesla"));
 }
 
-
-
 void loop()
 {
-  //serialEvent();
+  
+    
+  
+
   //Look for reports from the IMU
   if (myIMU.dataAvailable() == true)
   {
-    float x = myIMU.getMagX();
-    float y = myIMU.getMagY();
-    float z = myIMU.getMagZ();
-   //serial mag data
-    Serial.print(x,2);
-    Serial.print(" ");
-    Serial.print(y, 2);
-    Serial.print(" ");
-    Serial.print(z, 2);
-    Serial.print(" ");
-
-    BT.print(x,2);
-    BT.print(F(","));
-    BT.print(y,2);
-    BT.print(F(","));
-    BT.print(z,2);
-    BT.print(F(","));
-
-
-
-    //serial gyro data
-    float xg = myIMU.getGyroX();
-    float yg = myIMU.getGyroY();
-    float zg = myIMU.getGyroZ();
-    //Serial.print("Gyro: ");
-    Serial.print(xg, 2);
-    //Serial.print(F(","));
-    Serial.print(" ");
-    Serial.print(yg, 2);
-    //Serial.print(F(","));
-    Serial.print(" ");
-    Serial.print(zg, 2);
-    //Serial.print(F(","));
-    Serial.print(" ");
-    Serial.print("#");
-
-    //BT Gyro data
-//    BT.print("Gyro:  ");
-    BT.print(xg,2);
-    BT.print(F(","));
-    BT.print(yg,2);
-    BT.print(F(","));
-    BT.print(zg,2);
-    BT.print(F(","));
+    float mx = myIMU.getMagX();
+    float my = myIMU.getMagY();
+    float mz = myIMU.getMagZ();
     
-//    delay(1000);
-    float xA = myIMU.getAccelX();
-    float yA = myIMU.getAccelY();
-    float zA = myIMU.getAccelZ();
-   //serial mag data
-    Serial.print(xA,2);
-    Serial.print(" ");
-    Serial.print(yA, 2);
-    Serial.print(" ");
-    Serial.print(zA, 2);
-    Serial.print(" ");
+    float gx = myIMU.getGyroX();
+    float gy = myIMU.getGyroY();
+    float gz = myIMU.getGyroZ();
 
-    BT.print(xA,2);
-    BT.print(F(","));
-    BT.print(yA,2);
-    BT.print(F(","));
-    BT.print(zA,2);
-    BT.print(F(","));
-    BT.println("");
+
+    float ax = myIMU.getAccelX();
+    float ay = myIMU.getAccelY();
+    float az = myIMU.getAccelZ();
+
+    float quatI = myIMU.getQuatI();
+    float quatJ = myIMU.getQuatJ();
+    float quatK = myIMU.getQuatK();
+    float quatReal = myIMU.getQuatReal();
+//
+//    Serial.print("Time: ");
+      myTime = millis();
+
+      
+
+    
+//    Serial.print("Gyro : ");
+//    Serial.print(gx, 2);
+//    Serial.print(F(" "));
+//    Serial.print(gy, 2);
+//    Serial.print(F(" "));
+//    Serial.print(gz, 2);
+//    Serial.print(F(" "));
+//    Serial.println();
+//
+//    Serial.print("Mag : ");
+//    Serial.print(mx, 2);
+//    Serial.print(F(" "));
+//    Serial.print(my, 2);
+//    Serial.print(F(" "));
+//    Serial.print(mz, 2);
+//    Serial.print(F(" "));
+//    Serial.println();
+//    
+//    Serial.print("Acc : ");
+//    Serial.print(ax, 2);
+//    Serial.print(F(" "));
+//    Serial.print(ay, 2);
+//    Serial.print(F(" "));
+//    Serial.print(az, 2);
+//    Serial.print(F(" "));
+//    Serial.println();
+//
+//    Serial.print("Quat : ");
+//    Serial.print(quatI, 2);
+//    Serial.print(F(" "));
+//    Serial.print(quatJ, 2);
+//    Serial.print(F(" "));
+//    Serial.print(quatK, 2);
+//    Serial.print(F(" "));
+//    Serial.print(quatReal, 2);
+//    Serial.print(F(" "));
+//    Serial.println();
+//
+//
+      BT.print(gx,2);
+      BT.print(F(","));
+      BT.print(gy,2);
+      BT.print(F(","));
+      BT.print(gz,2);
+      BT.print(F(","));
+  
+      BT.print(mx,2);
+      BT.print(F(","));
+      BT.print(my,2);
+      BT.print(F(","));
+      BT.print(mz,2);
+      BT.print(F(","));
+  
+      BT.print(ax,2);
+      BT.print(F(","));
+      BT.print(ay,2);
+      BT.print(F(","));
+      BT.print(az,2);
+      BT.print(F(","));
+  
+      BT.print(quatI,2);
+      BT.print(F(","));
+      BT.print(quatJ,2);
+      BT.print(F(","));
+      BT.print(quatK,2);
+      BT.print(F(","));
+      BT.print(quatReal,2);
+      BT.print(F(","));
+
+      BT.print(myTime); // prints time since program started
+      BT.print(F(","));
+      
+      BT.println();
+
+
+
+
+    
   }
 }
