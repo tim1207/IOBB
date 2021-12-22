79c1c1cfbd119f35b38404e9de11c1523bac1d14
tim1207 <timlin891207@gmail.com>
2021-12-22 10:26:21 +0800

    add comTime&14DataForOne

--- StepSensor/StepSensor.ino
+++ StepSensor/StepSensor.ino
@@ -1,10 +1,17 @@
 #include <Wire.h>
+
 #include <stdlib.h>
+
 #include <stdint.h>
+
 #include <string.h>
+
 #include <pt.h>
+
 #include <BluetoothSerial.h>
+
 #include "SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
+
 BluetoothSerial BT;
 //#include <SoftwareSerial.h> //Serial transport for bluetooth
 //SoftwareSerial AT; //set bluetooth name as BT
@@ -13,12 +20,11 @@ unsigned long myTime;
 String receivedCommand;
 bool receiveComplete;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println("BNO080 Read Example");
-  BT.begin("Sensor");
+  BT.begin("KWK");
 
   Wire.begin();
 
@@ -34,27 +40,21 @@ void setup()
   myIMU.calibrateAll(); //Turn on cal for Accel, Gyro, and Mag
 
   //Enable Game Rotation Vector output
-  myIMU.enableGameRotationVector(100); //Send data update every 100ms
+  myIMU.enableGameRotationVector(50); //Send data update every 100ms
 
   //Enable Magnetic Field output
-  myIMU.enableMagnetometer(100); //Send data update every 100ms
-
-  
-
-  //Once magnetic field is 2 or 3, run the Save DCD Now command
-  Serial.println(F("Calibrating. Press 's' to save to flash"));
-  Serial.println(F("Output in form x, y, z, in uTesla"));
+  myIMU.enableMagnetometer(50); //Send data update every 100ms
 }
 
 
-void resetCommand(){
+void resetCommand() {
   receivedCommand = "";
   receiveComplete = false;
 }
 void serialEvent() {
-  while (BT.available()){
-    char inChar = (char)BT.read();
-    if ((inChar == '\n')||(inChar == '#')){
+  while (BT.available()) {
+    char inChar = (char) BT.read();
+    if ((inChar == '\n') || (inChar == '#')) {
       receiveComplete = (receivedCommand.length() > 0);
       break;
     } else {
@@ -64,81 +64,75 @@ void serialEvent() {
 }
 
 
-void loop()
-{
+void loop() {
+  serialEvent();
+  if (receiveComplete) {
+    Serial.println(receivedCommand);
+    for (int i = 0; i < receivedCommand.length(); i++)
+      BT.write((uint8_t) receivedCommand[i]);
+    //        BT.print((uint8_t)receivedCommand[0]);
+    resetCommand();
+    //Look for reports from the IMU
+    if (myIMU.dataAvailable() == true) {
+      float mx = myIMU.getMagX();
+      float my = myIMU.getMagY();
+      float mz = myIMU.getMagZ();
 
-    serialEvent();
-    if(receiveComplete)
-    {
-        Serial.println(receivedCommand);
-        for(int i=0;i<receivedCommand.length();i++)
-          BT.write((uint8_t)receivedCommand[i]);
-//        BT.print((uint8_t)receivedCommand[0]);
-        resetCommand();
-        //Look for reports from the IMU
-        if (myIMU.dataAvailable() == true)
-        {    
-            float mx = myIMU.getMagX();
-            float my = myIMU.getMagY();
-            float mz = myIMU.getMagZ();
+      float gx = myIMU.getGyroX();
+      float gy = myIMU.getGyroY();
+      float gz = myIMU.getGyroZ();
 
-            float gx = myIMU.getGyroX();
-            float gy = myIMU.getGyroY();
-            float gz = myIMU.getGyroZ();
+      float ax = myIMU.getAccelX();
+      float ay = myIMU.getAccelY();
+      float az = myIMU.getAccelZ();
 
-            float ax = myIMU.getAccelX();
-            float ay = myIMU.getAccelY();
-            float az = myIMU.getAccelZ();
+      float quatI = myIMU.getQuatI();
+      float quatJ = myIMU.getQuatJ();
+      float quatK = myIMU.getQuatK();
+      float quatReal = myIMU.getQuatReal();
+      myTime = millis();
 
-            float quatI = myIMU.getQuatI();
-            float quatJ = myIMU.getQuatJ();
-            float quatK = myIMU.getQuatK();
-            float quatReal = myIMU.getQuatReal();
-            myTime = millis();
+      // if(mx!=NULL and my!=NULL  and mz!=NULL  and gx!=NULL  and gy!=NULL  and gz!=NULL  and ax!=NULL  and ay!=NULL  and az!=NULL  and quatI!=NULL  and quatJ!=NULL  and quatK!=NULL  and quatReal!=NULL)
+      // {
 
-            // if(mx!=NULL and my!=NULL  and mz!=NULL  and gx!=NULL  and gy!=NULL  and gz!=NULL  and ax!=NULL  and ay!=NULL  and az!=NULL  and quatI!=NULL  and quatJ!=NULL  and quatK!=NULL  and quatReal!=NULL)
-            // {
-            
-                BT.print(F(","));
-                BT.print(gx,2);
-                BT.print(F(","));
-                BT.print(gy,2);
-                BT.print(F(","));
-                BT.print(gz,2);
-                BT.print(F(","));
-            
-                BT.print(mx,2);
-                BT.print(F(","));
-                BT.print(my,2);
-                BT.print(F(","));
-                BT.print(mz,2);
-                BT.print(F(","));
-            
-                BT.print(ax,2);
-                BT.print(F(","));
-                BT.print(ay,2);
-                BT.print(F(","));
-                BT.print(az,2);
-                BT.print(F(","));
-            
-                BT.print(quatI,2);
-                BT.print(F(","));
-                BT.print(quatJ,2);
-                BT.print(F(","));
-                BT.print(quatK,2);
-                BT.print(F(","));
-                BT.print(quatReal,2);
-                BT.print(F(","));
-        
-                BT.print(myTime); // prints time since program started
-                BT.print(F(","));
-                
-                BT.println();
-            // }
-            // else{
-            //     BT.println("No data");
-            //     Serial.println("No data");
-            // }         
-        }
+      BT.print(F(","));
+      BT.print(gx, 2);
+      BT.print(F(","));
+      BT.print(gy, 2);
+      BT.print(F(","));
+      BT.print(gz, 2);
+      BT.print(F(","));
+
+      BT.print(mx, 2);
+      BT.print(F(","));
+      BT.print(my, 2);
+      BT.print(F(","));
+      BT.print(mz, 2);
+      BT.print(F(","));
+
+      BT.print(ax, 2);
+      BT.print(F(","));
+      BT.print(ay, 2);
+      BT.print(F(","));
+      BT.print(az, 2);
+      BT.print(F(","));
+
+      BT.print(quatI, 2);
+      BT.print(F(","));
+      BT.print(quatJ, 2);
+      BT.print(F(","));
+      BT.print(quatK, 2);
+      BT.print(F(","));
+      BT.print(quatReal, 2);
+      BT.print(F(","));
+
+      BT.print(myTime); // prints time since program started
+      BT.print(F(","));
+
+      BT.println();
+     
+
+
     }
+  }
 }
